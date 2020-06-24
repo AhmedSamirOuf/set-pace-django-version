6f2378190043aef2b71437dff82f5decad2f2bed
AhmedSamirOuf <samir@devsquads.com>
2020-06-24 14:00:24 +0200

    create book model

--- book/models.py
+++ book/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=30, default='samir')
+    publisher = models.CharField(max_length=100, default='samir')
+    vendors = models.TextField(null=True, max_length=30)
+    types = models.TextField(null=True, max_length=15)
+    category = models.CharField(max_length=15, default='thriller')
--- book/tests.py
+++ book/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from .models import Book
 
-# Create your tests here.
+
+class TestBookModel(TestCase):
+
+    def set_test_param(self, param):
+        self.test_param = param
+
+    def test_book_has_a_title(self):
+        self.set_test_param('test_book')
+        book = Book(title=self.test_param)
+        self.assertEqual(book.title, self.test_param)
+
+    def test_book_has_author(self):
+        self.set_test_param('test_author')
+        book = Book(author=self.test_param)
+        self.assertEqual(book.author, self.test_param)
+
+    def test_book_has_publisher(self):
+        self.set_test_param('test_publisher')
+        book = Book(publisher=self.test_param)
+        self.assertEqual(book.publisher, self.test_param)
+
+    def test_book_has_vendors(self):
+        self.set_test_param(['test_vendor1', 'test_vendor2'])
+        book = Book(vendors=self.test_param)
+        self.assertEqual(book.vendors, self.test_param)
+
+    def test_book_has_types(self):
+        self.set_test_param(['test_type1', 'test_type2'])
+        book = Book(types=self.test_param)
+        self.assertEqual(book.types, self.test_param)
+
+    def test_book_has_category(self):
+        self.set_test_param('test_category')
+        book = Book(category=self.test_param)
+        self.assertEqual(book.category, self.test_param)
